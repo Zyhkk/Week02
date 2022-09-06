b3aca9e9402b5582b0fe180ce07e0dd55fb681c5
Eric 1329-FA <111894460+Zyhkk@users.noreply.github.com>
2022-09-06 09:33:12 -0500

    Create income.kt

---
+++ income.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var name = "John Smith"
+    var age = 20
+    var annualPay = 73000.00
+    
+    println("My name is $name and I am $age years old.")
+    println("I hope to make $annualPay per year.")
+    
+}
