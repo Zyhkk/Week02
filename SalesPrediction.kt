7ff377d475869e801a6e4525ec0b817cba1d54fe
Eric 1329-FA <111894460+Zyhkk@users.noreply.github.com>
2022-09-06 10:05:42 -0500

    Create SalesPrediction.kt

---
+++ SalesPrediction.kt
@@ -0,0 +1,8 @@
+fun main() {
+	var annualSales = 4.6
+    var salesDivision = 0.62
+    
+    var salesdivisionSales = annualSales * salesDivision
+    
+    println("The East Coast sales division made $salesdivisionSales million in sales this year out of a total of $annualSales million.")
+    }
